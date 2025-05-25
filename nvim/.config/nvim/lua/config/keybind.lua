859dd4bea25a78b9630f19e8f3f7e2e880a0f393
Sidati <sidatnouhi@gmail.com>
2025-05-25 17:08:06 -0100

    changed my setup to globalize my env
    .

---
+++ nvim/.config/nvim/lua/config/keybind.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set('i', '<S-j>', '<Esc>')
+vim.o.clipboard = 'unnamedplus'
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,4 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
